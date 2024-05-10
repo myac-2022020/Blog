c2c85f812a54b6cd93922abb37284a3a16eaf2b8
myac-2022020@kinal.edu.gt <myac-2022020@kinal.edu.gt>
2024-05-10 02:25:31 -0600

    Se subio blog del programador

--- BlogProgramador/src/App.jsx
+++ BlogProgramador/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { routes } from './routes.jsx'
+import { useRoutes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const element = useRoutes(routes)
 
   return (
-    <></>
+    <>
+      {element}
+    </>
   )
 }
 
